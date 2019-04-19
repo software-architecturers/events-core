021468128acbf9f41d1074ed7cda2025ac2de89b
Славик Винницкий <andrey.belyi1@gmail.com>
2019-04-19 20:56:19 +0300

    refactoring & code style checking

--- src/main/java/com/kpi/events/model/repository/EventRepository.java
+++ src/main/java/com/kpi/events/model/repository/EventRepository.java
@@ -3,20 +3,11 @@ package com.kpi.events.model.repository;
 import com.kpi.events.model.Event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
 import java.util.List;
 
-@Repository("eventRepository")
 public interface EventRepository extends JpaRepository<Event, Long> {
 
-//    @Query("update event set where event.id=:id")
-//    void updateEventById(Event event, long id);
-
     List<Event> findEventByDescriptionContainingOrTitleContaining(String search, String searchInTitle);
-
 
 }
