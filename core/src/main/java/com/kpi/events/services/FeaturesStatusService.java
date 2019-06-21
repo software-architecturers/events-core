6eecb21c4c6fc17ae417ba8487897e76ccf0d32a
Славик Винницкий <andrey.belyi1@gmail.com>
2019-06-21 18:35:01 +0300

    fix feature endpoint

--- core/src/main/java/com/kpi/events/services/FeaturesStatusService.java
+++ core/src/main/java/com/kpi/events/services/FeaturesStatusService.java
@@ -19,14 +19,16 @@ public class FeaturesStatusService {
         List<JSONObject> eventsStatusList = new ArrayList<>();
         JSONObject eventsStatus = new JSONObject();
         eventsStatus.put("features.events.Main", featuresStatusDto.getEventStatus());
-        eventsStatus.put("request", featuresStatusDto.getEventStatus());
-        eventsStatus.put("titleName", "Мисс КПИ");
+        eventsStatus.put("isActive", featuresStatusDto.getEventStatus());
+        eventsStatus.put("request", "http://goggle.com");
+        eventsStatus.put("titleName", "Все ивенты");
         eventsStatus.put("logoImageURL", "http://..");
         eventsStatusList.add(eventsStatus);
 
         JSONObject eventsMissKpiStatus = new JSONObject();
         eventsMissKpiStatus.put("features.events.Miss_Kpi", featuresStatusDto.getEventMissKpiStatus());
-        eventsMissKpiStatus.put("request", featuresStatusDto.getEventStatus());
+        eventsMissKpiStatus.put("isActive", featuresStatusDto.getEventMissKpiStatus());
+        eventsMissKpiStatus.put("request", "http://goggle.com");
         eventsMissKpiStatus.put("titleName", "Мисс КПИ");
         eventsMissKpiStatus.put("logoImageURL", "http://..");
         eventsStatusList.add(eventsMissKpiStatus);
