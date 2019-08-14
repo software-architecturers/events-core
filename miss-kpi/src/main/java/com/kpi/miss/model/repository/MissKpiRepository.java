3dfd53c23d073d13232e5b291e30c9c364282b5a
Slavick-Vinnitskyi <andrey.belyi1@gmail.com>
2019-08-14 19:37:34 +0300

    delete miss kpi

--- miss-kpi/src/main/java/com/kpi/miss/model/repository/MissKpiRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kpi.miss.model.repository;
-
-import com.kpi.miss.model.MissKpiEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MissKpiRepository extends JpaRepository<MissKpiEntity, Long> {
-
-
-}
