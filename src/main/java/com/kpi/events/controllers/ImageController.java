a5f47f31fb7c4f6c3d9dacad4df5314f47763f58
Slavick-Vinnitskyi <andrey.belyi1@gmail.com>
2019-08-15 15:15:09 +0300

    deleting previous approach of storing files

--- src/main/java/com/kpi/events/controllers/ImageController.java
+++ src/main/java/com/kpi/events/controllers/ImageController.java
@@ -1,73 +1,12 @@
 package com.kpi.events.controllers;
 
-import com.kpi.events.model.dto.file.UploadFileResponse;
 import com.kpi.events.services.AmazonClient;
-import com.kpi.events.services.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class ImageController {
-
-//    @Autowired
-//    private FileStorageService fileStorageService;
-//
-//    @PostMapping(value = "/uploadFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-//    public UploadFileResponse uploadFile(@RequestParam("file") MultipartFile file) {
-//        String fileName = fileStorageService.storeFile(file);
-//
-//        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-//                .path("/getImage/")
-//                .path(fileName)
-//                .toUriString();
-//
-//        return new UploadFileResponse(fileName, fileDownloadUri,
-//                file.getContentType(), file.getSize());
-//    }
-//
-//    @PostMapping("/uploadMultipleFiles")
-//    public List<UploadFileResponse> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
-//        return Arrays.asList(files)
-//                .stream()
-//                .map(file -> uploadFile(file))
-//                .collect(Collectors.toList());
-//    }
-//
-//    @GetMapping("/getImage/{fileName:.+}")
-//    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
-//        // Load file as Resource
-//        Resource resource = fileStorageService.loadFileAsResource(fileName);
-//
-//        // Try to determine file's content type
-//        String contentType = null;
-//        try {
-//            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-//        } catch (IOException ex) {
-//            System.out.println("Could not determine file type.");
-//        }
-//
-//        // Fallback to the default content type if type could not be determined
-//        if(contentType == null) {
-//            contentType = "application/octet-stream";
-//        }
-//
-//        return ResponseEntity.ok()
-//                .contentType(MediaType.parseMediaType(contentType))
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-//                .body(resource);
-//    }
 
     @Autowired
     private AmazonClient amazonClient;
