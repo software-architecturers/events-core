e89fd2b46c45331468b5bc57e7a78124951b400b
Slavick-Vinnitskyi <andrey.belyi1@gmail.com>
2019-08-16 23:37:47 +0300

    remove specifying scan packaging

--- src/main/java/com/kpi/events/Application.java
+++ src/main/java/com/kpi/events/Application.java
@@ -1,23 +1,21 @@
 package com.kpi.events;
 
-import com.kpi.events.model.files.FileStorageProperties;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(scanBasePackages = {"com.kpi.events"})
-@EnableJpaRepositories(
-        basePackages = {"com.kpi.events.model.repository"})
-@EntityScan(basePackages = {"com.kpi.miss", "com.kpi.events"})
-@EnableConfigurationProperties({
-        FileStorageProperties.class
-})
+//@SpringBootApplication(scanBasePackages = {"com.kpi.events"})
+@SpringBootApplication
+@EnableJpaRepositories
+//@EnableJpaRepositories(
+//        basePackages = {"com.kpi.events.model.repository"})
+//@EntityScan(basePackages = {"com.kpi.miss", "com.kpi.events"})
+
 public class Application {
 
     public static void main(String[] args) {
